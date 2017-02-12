2aee9e0056ef5f166cc073d9a3d353604fe2482f
bj paat <bjpaat@dailywarrior.ph>
2017-02-12 16:50:15 +0800

    Switch markdown to python-markdown2

--- simple_markdown_tag/templatetags/markdown.py
+++ simple_markdown_tag/templatetags/markdown.py
@@ -4,13 +4,20 @@ import json
 
 register = template.Library()
 
-@register.filter
-def markdown(value):
-    """ Turn markdown text into html """
+def markdown_old(value):
+    """ Turn markdown text into html using the github api"""
     url = "https://api.github.com/markdown"
     data = {
             "text": value,
+            "mode": "gfm",
             }
     data_json = json.dumps(data)
     r = requests.post(url, data=data_json)
     return r.text
+
+import markdown2
+
+@register.filter
+def markdown(value):
+    """ Turn markdown text into html using python-markdown2 """
+    return markdown2.markdown(value)
